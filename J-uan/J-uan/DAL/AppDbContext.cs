326211edc40506e3bf84a3020ee6801ded81f841
ibrahimiskandar <7nxjjfg@code.edu.az>
2022-06-14 18:50:21 +0400

    awdawd

---
+++ J-uan/J-uan/DAL/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using J_uan.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;     
+
+namespace J_uan.DAL 
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+         
+        public DbSet<Slider> Sliders { get; set; }
+    }
+}
