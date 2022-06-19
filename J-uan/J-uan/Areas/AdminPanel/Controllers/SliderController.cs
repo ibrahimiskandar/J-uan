c4e646a7872d9ee4dde203469b63a0ef268a5528
ibrahimiskandar <7nxjjfg@code.edu.az>
2022-06-20 03:00:31 +0400

    babat

--- J-uan/J-uan/Areas/AdminPanel/Controllers/SliderController.cs
+++ J-uan/J-uan/Areas/AdminPanel/Controllers/SliderController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using J_uan.DAL;
 using Microsoft.AspNetCore.Mvc;
 
 namespace J_uan.Areas.AdminPanel.Controllers
@@ -9,9 +10,14 @@ namespace J_uan.Areas.AdminPanel.Controllers
     [Area("AdminPanel")]
     public class SliderController : Controller
     {
+        private AppDbContext _context { get; }
+        public SliderController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_context.Slides);
         }
     }
 }
